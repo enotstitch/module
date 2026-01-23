5919476085e77484c2d63dc12a26e07cdec47355
Anna Kessler <kessler.anna96@gmail.com>
2026-01-24 01:19:24 +0300

    Вёрстка и адаптация секции "Проекты"

--- assets/js/scripts/sliders.js
+++ assets/js/scripts/sliders.js
@@ -10,7 +10,7 @@ export function initSliders() {
 			0: {
 				slidesPerView: 3,
 				pagination: {
-					el: '.swiper-pagination',
+					el: '.partners__pagination',
 					clickable: true,
 				},
 				navigation: false,
@@ -25,6 +25,39 @@ export function initSliders() {
 			},
 			992: {
 				slidesPerView: 6,
+				pagination: false,
+				navigation: {
+					nextEl: '.swiper-button-next',
+					prevEl: '.swiper-button-prev',
+				},
+			},
+		},
+	});
+
+	new Swiper('.projects-slider', {
+		modules: [Navigation, Pagination],
+		slidesPerView: 3,
+		spaceBetween: 40,
+
+		breakpoints: {
+			0: {
+				slidesPerView: 1,
+				pagination: {
+					el: '.projects__pagination',
+					clickable: true,
+				},
+				navigation: false,
+			},
+			769: {
+				slidesPerView: 2,
+				pagination: false,
+				navigation: {
+					nextEl: '.swiper-button-next',
+					prevEl: '.swiper-button-prev',
+				},
+			},
+			1200: {
+				slidesPerView: 3,
 				pagination: false,
 				navigation: {
 					nextEl: '.swiper-button-next',
