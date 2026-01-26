20928333dba766b6d70de917910d49b4417a2b4f
Anna Kessler <kessler.anna96@gmail.com>
2026-01-26 12:21:06 +0300

    Вёрстка и натяжка секции "Вопросы"

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,8 @@
 import { initHeader } from './modules/header/header.js';
 import { initSliders } from './scripts/sliders.js';
+import { Spollers } from './modules/spollers/spollers.js';
 
 initHeader();
 initSliders();
+
+new Spollers('questions-spollers');
