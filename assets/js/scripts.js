c9b547a598d3a6bafeadb7e515443d2f31f52f0f
Anna Kessler <kessler.anna96@gmail.com>
2026-01-27 18:54:25 +0300

    Вёрстка и адаптация страницы "Продукты" под WordPress

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,16 @@
 import { initHeader } from './modules/header/header.js';
 import { initSliders } from './scripts/sliders.js';
 import { Spollers } from './modules/spollers/spollers.js';
+import { tabs } from './modules/tabs/tabs.js';
 
 initHeader();
 initSliders();
 
 new Spollers('questions-spollers');
 new Spollers('vacancies-spollers');
+
+// ПЕРВЫЙ аргумент - класс всего нашего хедера табов.
+// ВТОРОЙ аргумент - класс конкретного элемента, при клике на который будет переключатся таб.
+// ТРЕТИЙ аргумент - класс того блока, который будет переключаться.
+// ЧЕТВЕРТЫЙ аргумент - класс активности, который будет добавлятся для таба, который сейчас активен.
+tabs('.products-tabs__header', '.tabs__header-item', '.tabs__content-item', 'active');
